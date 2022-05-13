3384368656e97682c271b31fd34513f0ce59564b
Rodrigo Abreu <rodrigo.telles.abreu@gmail.com>
2022-05-13 06:53:58 -0300

    Urna Eletronica
    Projeto Urna

---
+++ Projeto_Urna_Eletronica.py
@@ -0,0 +1,17 @@
+print("**"*12)
+print("\tURNA ELETRONICA")
+print("**"*12)
+
+try:
+    titulo = int(input("Informe seu Titulo: "))
+
+except ValueError:
+
+    print("Informe apenas valores NUMÉRICOS.")
+
+print("**"*12)
+print("\tCANDIDATOS")
+print("**"*12)
+
+
+print("\n[17] - Bolsonaro")
