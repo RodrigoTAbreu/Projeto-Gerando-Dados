17732813ea020ae5ce996e987319b569ee3d2c32
Rodrigo Abreu <rodrigo.telles.abreu@gmail.com>
2022-09-21 06:47:38 -0300

    vscode

--- scatter_squares.py
+++ scatter_squares.py
@@ -8,7 +8,7 @@ y_values = [1,4,9,16,25]
 x_values = list(range(1,1001))
 y_values = [x**2 for x in x_values]
 
-plt.scatter(x_values,y_values, s=10)
+plt.scatter(x_values,y_values, s=20)
 
 #Define o título do gráfico e nomeia os eixos
 plt.title("Numeros Quadrados", fontsize=24)
