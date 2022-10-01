8a27aae60fe9d0cf8837cdda5c882a18c6b83f5f
Rodrigo Abreu <rodrigo.telles.abreu@gmail.com>
2022-10-01 06:21:38 -0300

    Menu

--- consultaClientes/rompimento_massivo.py
+++ consultaClientes/rompimento_massivo.py
@@ -5,17 +5,27 @@ from time import sleep
 while True:
     resposta = menu(['OLT', 'CTO', 'BAIRRO', 'CONDOMÍNIO', 'SAIR'])
     if resposta == 1:
+        print(linha())
         equipamento()
+        sleep(1)
     elif resposta == 2:
+        print(linha())
         consulta_cto()
+        sleep(1)
     elif resposta == 3:
+        print(linha())
         consulta_bairro()
+        sleep(1)
     elif resposta == 4:
+        print(linha())
         consultaCond()
+        sleep(1)
     elif resposta == 5:
         print('Encerrando Consultas !!!')
+        print(linha())
+        sleep(2)
         break
     else:
         print('ERRO! Digite uma opção válida!')
-    sleep(2)
+        sleep(2)
 
